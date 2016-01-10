e4d8ec81b70462d04f57cca7d5df0ed116145945
vSkyD1 <vskyd1@gmail.com>
2016-01-10 23:26:43 +0600

    update

--- src/main/java/binnie/core/AbstractMod.java
+++ src/main/java/binnie/core/AbstractMod.java
@@ -1,37 +1,42 @@
 package binnie.core;
 
-import binnie.Binnie;
-import binnie.core.gui.IBinnieGUID;
-import binnie.core.mod.parser.FieldParser;
-import binnie.core.network.IPacketID;
-import binnie.core.network.IPacketProvider;
-import binnie.core.network.packet.MessageBinnie;
-import binnie.core.proxy.IProxyCore;
-import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
+import binnie.core.network.packet.MessageBinnie;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import java.util.Iterator;
+import binnie.core.mod.parser.FieldParser;
+import binnie.Binnie;
+import binnie.core.network.BinniePacketHandler;
+import binnie.core.proxy.IProxyCore;
+import binnie.core.gui.IBinnieGUID;
+import binnie.core.network.IPacketID;
 import net.minecraftforge.common.MinecraftForge;
-
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
+import java.lang.reflect.Field;
+import java.util.LinkedHashSet;
+import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import binnie.core.network.IPacketProvider;
 
-public abstract class AbstractMod implements IPacketProvider, IInitializable {
+public abstract class AbstractMod implements IPacketProvider, IInitializable
+{
     private SimpleNetworkWrapper wrapper;
-    private LinkedHashSet fields = new LinkedHashSet();
-    protected List modules = new ArrayList();
+    private LinkedHashSet<Field> fields;
+    protected List<IInitializable> modules;
 
     public AbstractMod() {
-        super();
+        this.fields = new LinkedHashSet<Field>();
+        this.modules = new ArrayList<IInitializable>();
         BinnieCore.registerMod(this);
-        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register((Object)this);
     }
 
     public abstract boolean isActive();
 
+    @Override
     public abstract String getChannel();
 
+    @Override
     public IPacketID[] getPacketIDs() {
         return new IPacketID[0];
     }
@@ -52,92 +57,87 @@ public abstract class AbstractMod implements IPacketProvider, IInitializable {
         return this.wrapper;
     }
 
-    protected abstract Class getPacketHandler();
+    protected abstract Class<? extends BinniePacketHandler> getPacketHandler();
 
+    @Override
     public void preInit() {
-        if (this.isActive()) {
-            if (this.getConfigs() != null) {
-                for (Class cls : this.getConfigs()) {
-                    Binnie.Configuration.registerConfiguration(cls, this);
-                }
+        if (!this.isActive()) {
+            return;
+        }
+        if (this.getConfigs() != null) {
+            for (final Class cls : this.getConfigs()) {
+                Binnie.Configuration.registerConfiguration(cls, this);
             }
-
-            this.getProxy().preInit();
-
-            for (IInitializable module : this.modules) {
-                module.preInit();
+        }
+        this.getProxy().preInit();
+        for (final IInitializable module : this.modules) {
+            module.preInit();
+        }
+        for (final Field field : this.getClass().getFields()) {
+            this.fields.add(field);
+        }
+        for (final Class cls : this.getClass().getClasses()) {
+            for (final Field field2 : this.getClass().getFields()) {
+                this.fields.add(field2);
             }
-
-            for (Field field : this.getClass().getFields()) {
-                this.fields.add(field);
+        }
+        for (final IInitializable module : this.modules) {
+            for (final Field field3 : module.getClass().getFields()) {
+                this.fields.add(field3);
             }
-
-            for (Class var10000 : this.getClass().getClasses()) {
-                for (Field field : this.getClass().getFields()) {
-                    this.fields.add(field);
-                }
+        }
+        for (final Field field4 : this.fields) {
+            try {
+                FieldParser.preInitParse(field4, this);
             }
-
-            for (IInitializable module : this.modules) {
-                for (Field field : module.getClass().getFields()) {
-                    this.fields.add(field);
-                }
+            catch (Exception e) {
+                throw new RuntimeException(e);
             }
-
-            for (Field field : this.fields) {
-                try {
-                    FieldParser.preInitParse(field, this);
-                } catch (Exception var9) {
-                    throw new RuntimeException(var9);
-                }
-            }
-
         }
     }
 
+    @Override
     public void init() {
-        if (this.isActive()) {
-            this.getProxy().init();
-            this.wrapper = NetworkRegistry.INSTANCE.newSimpleChannel(this.getChannel());
-            this.wrapper.registerMessage(this.getPacketHandler(), MessageBinnie.class, 1, Side.CLIENT);
-            this.wrapper.registerMessage(this.getPacketHandler(), MessageBinnie.class, 1, Side.SERVER);
-
-            for (IInitializable module : this.modules) {
-                module.init();
+        if (!this.isActive()) {
+            return;
+        }
+        this.getProxy().init();
+        (this.wrapper = NetworkRegistry.INSTANCE.newSimpleChannel(this.getChannel())).registerMessage((Class)this.getPacketHandler(), (Class)MessageBinnie.class, 1, Side.CLIENT);
+        this.wrapper.registerMessage((Class)this.getPacketHandler(), (Class)MessageBinnie.class, 1, Side.SERVER);
+        for (final IInitializable module : this.modules) {
+            module.init();
+        }
+        for (final Field field : this.fields) {
+            try {
+                FieldParser.initParse(field, this);
             }
-
-            for (Field field : this.fields) {
-                try {
-                    FieldParser.initParse(field, this);
-                } catch (Exception var4) {
-                    throw new RuntimeException(var4);
-                }
+            catch (Exception e) {
+                throw new RuntimeException(e);
             }
-
         }
     }
 
+    @Override
     public void postInit() {
-        if (this.isActive()) {
-            this.getProxy().postInit();
-
-            for (IInitializable module : this.modules) {
-                module.postInit();
+        if (!this.isActive()) {
+            return;
+        }
+        this.getProxy().postInit();
+        for (final IInitializable module : this.modules) {
+            module.postInit();
+        }
+        for (final Field field : this.fields) {
+            try {
+                FieldParser.postInitParse(field, this);
             }
-
-            for (Field field : this.fields) {
-                try {
-                    FieldParser.postInitParse(field, this);
-                } catch (Exception var4) {
-                    throw new RuntimeException(var4);
-                }
+            catch (Exception e) {
+                throw new RuntimeException(e);
             }
-
         }
     }
 
-    protected final void addModule(IInitializable init) {
+    protected final void addModule(final IInitializable init) {
         this.modules.add(init);
-        MinecraftForge.EVENT_BUS.register(init);
+        MinecraftForge.EVENT_BUS.register((Object)init);
     }
 }
--- src/main/java/binnie/core/resource/ManagerResource.java
+++ src/main/java/binnie/core/resource/ManagerResource.java
@@ -1,47 +1,46 @@
 package binnie.core.resource;
 
-import binnie.core.AbstractMod;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
-
+import binnie.core.AbstractMod;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ManagerResource {
-    private List icons = new ArrayList();
+public class ManagerResource
+{
+    private List<BinnieIcon> icons;
 
     public ManagerResource() {
-        super();
+        this.icons = new ArrayList<BinnieIcon>();
     }
 
-    public BinnieResource getPNG(AbstractMod mod, ResourceType type, String path) {
+    public BinnieResource getPNG(final AbstractMod mod, final ResourceType type, final String path) {
         return this.getFile(mod, type, path + ".png");
     }
 
-    public BinnieResource getFile(AbstractMod mod, ResourceType type, String path) {
+    public BinnieResource getFile(final AbstractMod mod, final ResourceType type, final String path) {
         return new BinnieResource(mod, type, path);
     }
 
-    public void registerIcon(BinnieIcon binnieIcon) {
+    public void registerIcon(final BinnieIcon binnieIcon) {
         this.icons.add(binnieIcon);
     }
 
-    public BinnieIcon getItemIcon(AbstractMod mod, String iconFile) {
+    public BinnieIcon getItemIcon(final AbstractMod mod, final String iconFile) {
         return new BinnieIcon(mod, ResourceType.Item, iconFile);
     }
 
-    public BinnieIcon getBlockIcon(AbstractMod mod, String iconFile) {
+    public BinnieIcon getBlockIcon(final AbstractMod mod, final String iconFile) {
         return new BinnieIcon(mod, ResourceType.Block, iconFile);
     }
 
     @SideOnly(Side.CLIENT)
-    public void registerIcons(IIconRegister register, int type) {
-        for (BinnieIcon icon : this.icons) {
+    public void registerIcons(final IIconRegister register, final int type) {
+        for (final BinnieIcon icon : this.icons) {
             if (icon.getTextureSheet() == type) {
                 icon.registerIcon(register);
             }
         }
-
     }
 }
